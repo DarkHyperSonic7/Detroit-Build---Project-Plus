1e47308b46faf79f767317dcd77c19072e3d529b
Gabriel Vargas <gabriel_sonic34@yahoo.com>
2026-09-13 23:19:28 -0400

    Project+ v3.2 Update
    Implemented P+ 3.2's core changes and some cosmetics as well.  Some costumes from the previous build have been preserved

--- Build/Project+/Source/ProjectM/Modifier/Hitlag.asm
+++ Build/Project+/Source/ProjectM/Modifier/Hitlag.asm
@@ -12,19 +12,15 @@ HOOK @ $80772B90
 
 #####################################################
 Projectiles Can Experience Hitstop [Magus, DukeItOut]
+#
+# Requires Heritage.asm!
 #####################################################
 HOOK @ $808E53B4
 {
 	lbz r0, 0x21(r27)				# Collision Hit Type
 	cmpwi r0, 3; beq- finish		# No hitlag when being reflected!
-	lwz r0, 0xB8(r26) 				# Get the weapon ID
-	cmpwi r0, 3; beq- checkHitstun 	# Link's Boomerang
-	cmpwi r0, 5; beq- checkHitstun 	# Link's Arrows
-	cmpwi r0, 23; beq- checkHitstun # Snake's Cypher
-	cmpwi r0, 140; beq- checkHitstun# Toon Link's Arrows
-	cmpwi r0, 141; bne+ finish		# Toon Link's Boomerang
-
 checkHitstun:
+<<<<<<< Updated upstream
 	lwz r12, 0x90(r26)		# \
 	lwz r12, 0xC4(r12)		# |
 	mtctr r12				# | Get the creator of this projectile
@@ -40,8 +36,16 @@ checkHitstun:
 	li r5, -1				# \
 	bla 0x814F20			# / Get the fighter pointer
 		
+=======
+	lwz r3, 0x60(r26)
+	bla 0x3FF0				# Get fighter that spawned this.
+	cmpwi r3, 0				# \ null check
+	beq finish				# /
+	lwz r3, 0x8(r3)
+>>>>>>> Stashed changes
 	lwz r4, 0x110(r3) 		# Fighter Character ID
 	cmpwi r4, 0x05; bne+ notKirby # Kirby copy abilities will need to be checked!
+	
 	lwz r4, 0x60(r3)		# \
 	lwz r4, 0x70(r4)		# | Check the copy ability ID!
 	lwz r4, 0x20(r4)		# | It is in LA-Basic[72]
@@ -49,9 +53,15 @@ checkHitstun:
 	lwz r4, 0x120(r4)		# /
 	
 notKirby:
-	cmpwi r4, 0x02; beq+ haveHitstun # Link
-	cmpwi r4, 0x29; beq+ haveHitstun # Toon Link
-	cmpwi r4, 0x2E; bne- finish		 # Snake
+	lwz r0, 0xB8(r26)			# Get the weapon ID
+	rlwimi r0, r4, 8, 16, 23	# Move char ID to second byte of r0 while keeping projectile ID in lowest byte.
+	
+	cmplwi r0, 0x2E17; beq- haveHitstun # Snake's Cypher (23)
+	cmplwi r0, 0x0203; beq- haveHitstun	# Link's Boomerang (3)
+	cmplwi r0, 0x0205; beq- haveHitstun	# Link's Arrows (5)
+	cmplwi r0, 0x298C; beq- haveHitstun # Toon Link's Boomerang (140)
+	cmplwi r0, 0x298D; bne+ finish		# Toon Link's Boomerang (141)
+	
 	# These checks are here so that, in builds that clone characters,
 	# they can choose to opt-in on their own terms!
 	# The only clone I currently can think of that should add support
